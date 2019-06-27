e524ab382530d1c873dc1cee1654782ced42623e
Arifasiwi Milanda Putri <milandaputri05@gmail.com>
2019-06-27 07:05:49 +0700

    Update Responsi2.java

--- src/responsi2/Responsi2.java
+++ src/responsi2/Responsi2.java
@@ -30,16 +30,14 @@ public class Responsi2 { // nama kelas program untuk eksekusi
         //Memanggil kedua method getTanggal dan getWaktu untuk ditampilkan pada console
         Scanner inputan = new Scanner(System.in);
         //        inisialisasi pembayaran awal
-        int harga = 0; 
+        int harga = 0;
 
-        ArrayList hari = new ArrayList<>();
-        ArrayList namacd = new ArrayList<>();
-        ArrayList total = new ArrayList<>();
-        ArrayList kembalian = new ArrayList<>();
-        ArrayList nama = new ArrayList<>();
-        ArrayList tglkembali = new ArrayList<>();
+        Integer[] hari = new Integer[1];
+        String[] namacd = new String[10];
+        Integer[] kembalian = new Integer[1];
+        String nama[] = new String[1];
+        String tglkembali[] = new String[1];
 
-        int menu = 0;
         System.out.println("==========================================");
         System.out.println("             Rental CD Gamezone           ");
         System.out.println("==========================================");
@@ -53,29 +51,29 @@ public class Responsi2 { // nama kelas program untuk eksekusi
 
         System.out.print("Masukkan Nama Penyewa:  ");
         String sewa = inputan.next();
-        nama.add(sewa);
+        nama[0] = inputan.nextLine();
         System.out.println();
 
         System.out.print("Berapa banyak CD yang ingin anda sewa:  ");
         int banyak = inputan.nextInt();
 
-         for (int ulang = 1; ulang <= banyak; ulang++) {
+        for (int ulang = 1; ulang <= banyak; ulang++) {
             System.out.print("Masukkan No CD: ");
             String pilih = inputan.next();
             System.out.println("================");
 
             if (pilih.equals("1")) {
-                   
+
                 System.out.println("Nama CD yang dipinjam : Call Of Duty");
-                namacd.add("Call Of Duty");
+                namacd[ulang] = ("Call Of Duty");
 
                 System.out.println("Harga CD Call Of Duty : Rp. 20.000/Hari");
                 harga = harga + 20000;
 
                 System.out.println("================");
             } else if (pilih.equals("2")) {
-                System.out.println("Nama CD yang dipinjam : Resident Evil 6" );
-                namacd.add("Resident Evil 6");
+                System.out.println("Nama CD yang dipinjam : Resident Evil 6");
+                namacd[ulang] = ("Resident Evil 6");
 
                 System.out.println("Harga CD Resident Evil 6 : 25.000/Hari");
                 harga = harga + 25000;
@@ -83,7 +81,7 @@ public class Responsi2 { // nama kelas program untuk eksekusi
                 System.out.println("================");
             } else if (pilih.equals("3")) {
                 System.out.println("Nama CD yang dipinjam : Prototype 2");
-                namacd.add("Prototype 2");
+                namacd[ulang] = ("Prototype 2");
 
                 System.out.println("Harga CD Prototype 2 : 30.000/Hari");
                 harga = harga + 30000;
@@ -91,7 +89,7 @@ public class Responsi2 { // nama kelas program untuk eksekusi
                 System.out.println("================");
             } else if (pilih.equals("4")) {
                 System.out.println("Nama CD yang dipinjam : Hero 4");
-                namacd.add("Hero 4");
+                namacd[ulang] = ("Hero 4");
 
                 System.out.println("Harga CD Hero 4 : 40.000/Hari");
                 harga = harga + 40000;
@@ -99,60 +97,59 @@ public class Responsi2 { // nama kelas program untuk eksekusi
                 System.out.println("================");
             } else if (pilih.equals("5")) {
                 System.out.println("Nama CD yang dipinjam : Dead By Dylight");
-                namacd.add("Dead By Dylight");
+                namacd[ulang] = ("Dead By Dylight");
 
                 System.out.println("Harga CD Dead By Dylight : 35.000/Hari");
                 harga = harga + 35000;
 
                 System.out.println("================");
             }
-         }
-                
-                System.out.print("Berapa Lama Anda ingin meminjam : ");
-                int lama = inputan.nextInt();
-                hari.add(lama);
+        }
 
-                System.out.print("Tanggal pengembalian :");
-                cal.add(Calendar.DAY_OF_MONTH, lama);
-                System.out.println(sdf.format(cal.getTime()));
+        System.out.print("Berapa Lama Anda ingin meminjam : ");
+        hari[0] = inputan.nextInt();
 
-                int tot = harga * lama;
+        System.out.print("Tanggal pengembalian :");
+        cal.add(Calendar.DAY_OF_MONTH, hari[0]);
+        System.out.println(sdf.format(cal.getTime()));
 
-                System.out.println("Total pembayaran sebesar " +tot+".");
-                System.out.println();
+        int total = harga * hari[0];
 
-                System.out.print("Masukkan Uang Anda : ");
-                int bayar = inputan.nextInt();
+        System.out.println("Total pembayaran sebesar " + total + ".");
+        System.out.println();
 
-                int kembali1=0;
-                int x=1;
-                while (x!=0) {
-                if (tot > bayar) {
-                        System.out.print("Maaf Uang Anda Kurang,Tolong Masukkan dengan benar :");
-                        bayar = inputan.nextInt();
-                    
-                }else{
-                    kembali1 = bayar - tot;
-                    System.out.println("Kembalian Anda = " + kembali1);
-                    kembalian.add(kembali1);
-                    x=0;
-                }
-                }
-                    kembalian.add(kembali1);
-                    
-                    System.out.println(kembalian.get(0));
-                
+        System.out.print("Masukkan Uang Anda : ");
+        int bayar = inputan.nextInt();
+
+        int kembali1 = 0;
+        int x = 1;
+        while (x != 0) {
+            if (total > bayar) {
+                System.out.print("Maaf Uang Anda Kurang,Tolong Masukkan dengan benar :");
+                bayar = inputan.nextInt();
+
+            } else {
+                kembali1 = bayar - total;
+                System.out.println("Kembalian Anda = " + kembali1);
+                kembalian[0] = kembali1;
+                x = 0;
+            }
+        }
+        kembalian[0] = kembali1;
+
         System.out.println("==========================================");
         System.out.println("              STRUK PEMBAYARAN            ");
         System.out.println("==========================================");
 
-            System.out.println("Nama Penyewa: " + sewa);
-            System.out.println("Nama CD yang disewa: " + namacd);
-            System.out.print("Tanggal pengembalian :"); 
-            System.out.println(sdf.format(cal.getTime()));
-            System.out.println("Total pembayaran sebesar " +tot);
-            System.out.println("Kembalian Anda: " +kembali1);
-            System.out.println("Terimakasih atas kunjungannya.");
-            
+        System.out.println("Nama Penyewa: " + sewa);
+        System.out.println("Nama CD yang disewa: ");
+        for (int cd = 1; cd <= banyak; cd++) {
+            System.out.println(" -" + namacd[cd]);
+        }
+        System.out.print("Tanggal pengembalian :");
+        System.out.println(sdf.format(cal.getTime()));
+        System.out.println("Total pembayaran sebesar :" + total);
+        System.out.println("Kembalian Anda: " + kembalian[0]);
+        System.out.println("Terimakasih atas kunjungannya.");
     }
 }
